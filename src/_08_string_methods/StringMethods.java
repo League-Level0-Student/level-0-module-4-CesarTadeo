9ea2877db80333d054001822474bfd49b90eb1e1
League Student <league@iMac-27.attlocal.net>
2019-09-01 12:58:18 -0700

    string metjods, Pi aloud(IP)

--- src/_08_string_methods/StringMethods.java
+++ src/_08_string_methods/StringMethods.java
@@ -2,28 +2,36 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
-
-		// 2. Print the 3rd char of your String to the console.
-		//    HINT: .charAt
-		
+		String color = JOptionPane.showInputDialog("What is your favorite color");
+		System.out.println(color.charAt(2));
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+		System.out.println(color.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
+		for (int i = 0; i < color.length(); i++) {
+		System.out.println(color.charAt(i));	
+		}
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
-		//             for char 'b', then print "b is at index 1"
+			//             for char 'b', then print "b is at index 1"
+		
+		 for (int i = 0; i < color.length(); i++) {	
+			 if ( color.charAt(i) == 'r') {
+		System.out.println(i);
+					
+		 }
+		 }
+	
 		
 	}
 }
--- src/_09_pi_aloud/PiAloud.java
+++ src/_09_pi_aloud/PiAloud.java
@@ -9,9 +9,10 @@ import java.util.Scanner;
 public class PiAloud {
 
 	// 1. Make a main method and make sure your program can run
-
+	public static void main(String[] args) {
+	
 	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
-
+		String Pi = "1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
 	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
 
 	// 4. Print ALL the digits of of Pi (hint: use a loop)
@@ -28,6 +29,7 @@ public class PiAloud {
 			Runtime.getRuntime().exec("say " + characterToSpeak).waitFor();
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
 		}
 	}
 
